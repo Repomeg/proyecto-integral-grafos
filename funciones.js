554a939ffc654253ea0e9d7086185550baf3557e
LDonGithub <83192358+LDonGithub@users.noreply.github.com>
2021-07-08 18:37:51 -0400

    Version hasta matriz de distancias

--- funciones.js
+++ funciones.js
@@ -1,3 +1,12 @@
+//Prototipos
+Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+}
+
+Array.prototype.unique=function(a){
+    return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+});
+
 //Document Botones
 const btn0 = document.querySelector(".btn0");
 const btn1 = document.querySelector(".btn1");
@@ -9,15 +18,18 @@ let txtC_separado = [];
 let txtD; // --> Info Distribucion txt ingresado
 let txtD_separado = [];
 
+let arr_Cantcaminos = []; // --> Cantidad de caminos de distribucion separados en cada casilla de arr
+
 let lat1_ej = -53.783333;
 let lon1_ej = -67.7; 
 let lat2_ej = -54.807222; 
 let lon2_ej = -68.304444; 
 
+let matrizDistancias = []; // --> Distancia entre todos los puntos
+
 //Clases
 class puntos{
-    constructor(t,n,x,y){
-        this.t = [];
+    constructor(n,x,y){
         this.n = [];
         this.x = [];
         this.y = [];
@@ -32,15 +44,22 @@ class distribucion{
     }
 }
 
-const info_C = new puntos;
+//Clases Puntos Distribucion Y Ventas
+const Pdis = new puntos;
+const Pven = new puntos;
+
+//Clase Info Distribucion Objetos
 const info_D = new distribucion;
 
-//Funcion Guardar Info txt Coordenada 
+//Funcion Guardar Info txt Coordena
 const guardarTxtC = () => {
-    txtC = document.getElementById("pantalla").value;
+    txtC = null;
+    txtC = document.getElementById("pantalla-1").value;
 }
 
 const separarTxtC = () => {
+    txtC_separado = [];
+
     let aux = txtC.split('\n');
     let num = aux.length;
 
@@ -56,26 +75,44 @@ const guardarClassC = () => {
     let aux = [];
     let sep = [];
 
+    Pven.n = [];
+    Pven.x = [];
+    Pven.y = [];
+    Pdis.n = [];
+    Pdis.x = [];
+    Pdis.y = [];
+
     for(let l=0;l<numi;l++){
         aux.push(txtC_separado[l].replace(',',';'));
         sep.push(aux[l].split(';'));
     }
 
     for(let m=0;m<numi;m++){
-        info_C.t.push(sep[m][0]);
-        info_C.n.push(sep[m][1]);
-        info_C.x.push(Number.parseInt(sep[m][2]));
-        info_C.y.push(Number.parseInt(sep[m][3]));
+        console.log(sep[0]);
+        if(sep[m][0]=='P'){
+            Pven.n.push(sep[m][1]);
+            Pven.x.push(Number.parseInt(sep[m][2]));
+            Pven.y.push(Number.parseInt(sep[m][3]));
+        }
+        if(sep[m][0]=='C'){
+            Pdis.n.push(sep[m][1]);
+            Pdis.x.push(Number.parseInt(sep[m][2]));
+            Pdis.y.push(Number.parseInt(sep[m][3]));
+        }
     }
-    console.log(info_C);
+    console.log(Pven);
+    console.log(Pdis);
 }
 
 //Funcion Guardar Info txt Distribucion
 const guardarTxtD = () => {
+    txtD = null;
     txtD = document.getElementById("pantalla-dis").value;
 }
 
 const separarTxtD= () => {
+    txtD_separado = [];
+
     let aux = txtD.split('\n');
     let numi = aux.length;
 
@@ -88,10 +125,13 @@ const separarTxtD= () => {
 const guardarClassD = () => {
     let numo = txtD_separado.length;
 
-    let aux = [];
     let sep = [];
 
-    for(let ll=0;ll<numo;ll++){
+    info_D.c = [];
+    info_D.p = [];
+    info_D.n = [];
+
+    for(let ll=0;ll<numo;ll++){ 
         sep.push(txtD_separado[ll].split(';'));
     }
 
@@ -104,22 +144,66 @@ const guardarClassD = () => {
 }
 
 //Funcion Distancia entre puntos
-Number.prototype.toRad = function() {
-    return this * Math.PI / 180;
-}
-
-const distancia = (lat1,lon1,lat2,lon2) => { 
+const distancia = (Coor1,Coor2) => { 
     let R = 6371; // km 
 
-    let dLat = (lat2-lat1).toRad();  
-    let dLon = (lon2-lon1).toRad();  
+    let dLat = (Coor2[0]-Coor1[0]).toRad();  
+    let dLon = (Coor2[1]-Coor1[1]).toRad();  
     let a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                    Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                    Math.cos(Coor1[0].toRad()) * Math.cos(Coor2[0].toRad()) * 
                     Math.sin(dLon/2) * Math.sin(dLon/2);  
     let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
     let d = R * c; 
 
-    alert(d); 
+    return d;
+}
+
+//Funcion devuelve array de cant de puntos en cada camino
+const cantPuntosCamino = () => {
+    let num_envios  = info_D.c.length;
+    let num_cantC = Pdis.n.length;
+    
+    let arr_cenDis= JSON.parse(JSON.stringify(info_D.c));
+    let arr_centros= JSON.parse(JSON.stringify(Pdis.n));
+    //----------------------------------------------------
+
+    for(let i=0;i<num_cantC;i++){
+        let cont = 0;
+        for(let j=0;j<num_envios;j++){
+            if(arr_centros[i] == arr_cenDis[j]){
+                cont++;
+            }
+        }
+        arr_Cantcaminos.push(cont);   
+    }
+    console.log(arr_Cantcaminos);
+}
+
+//Funcion devuelve matriz con las distancias entre todos los pares de puntos
+const llenarInfoCaminos = () => {
+    let num = Pdis.n.length+Pven.n.length;
+    let matrizCoor = [];
+
+    for(let j=0; j<Pven.n.length; j++){
+        matrizCoor.push([Pven.x[j],Pven.y[j]]);
+    }
+    for(let i=0; i<Pdis.n.length; i++){
+        matrizCoor.push([Pdis.x[i],Pdis.y[i]]);
+    }
+    console.log(matrizCoor);
+    console.log(matrizCoor[0]);
+
+    let matrizDistancias = []; // --> Crea matriz cuadrada
+    for(let y=0; y<num; y++) {
+        matrizDistancias[y] = new Array;
+    }
+
+    for(let l=0; l<num; l++){
+        for(let m=0; m<num; m++){
+            matrizDistancias[l][m]=distancia(matrizCoor[l],matrizCoor[m]);
+        }
+    }
+    console.table(matrizDistancias);
 }
 
 //Botones
@@ -135,4 +219,6 @@ btn1.addEventListener('click', (evt) => {
     console.log(txtD);
     separarTxtD();
     guardarClassD();
+    cantPuntosCamino();
+    llenarInfoCaminos();
 })
